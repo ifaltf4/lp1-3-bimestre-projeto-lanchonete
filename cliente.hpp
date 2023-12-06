fd7440d2f67f73fc8f7c9d40cb7503e9fc631760
josé victor <138799380+ifaltf4@users.noreply.github.com>
2023-12-06 14:25:21 -0300

    do1 bimestre

--- cliente.hpp
+++ cliente.hpp
@@ -3,6 +3,14 @@
 #ifndef CLIENTE_HPP
 #define CLIENTE_HPP
 
+void fregues();
+void listarsandwichs();
+void listarsucos();
+void escolherlanche(std::string nome); 
+void calcularvalor();
+void listarcarinho();
+void removerdocarrinho(std::string nome);
+
 
 
 
