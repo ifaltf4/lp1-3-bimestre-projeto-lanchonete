2622502d35b1c68098ca1db217b7b766880895a2
josé victor <138799380+ifaltf4@users.noreply.github.com>
2023-12-06 14:17:07 -0300

    adicionado

--- menu.hpp
+++ menu.hpp
@@ -1,7 +1,26 @@
-#include <iostream>
+
 
 #ifndef MENU_HPP
 #define MENU_HPP
+
+#include <iostream>
+#include <string>
+
+class menu {
+
+protected:
+ std::string produto;
+ double valor;
+
+public:
+
+    menu(const std::string &produto, double valor);
+    virtual ~menu();
+
+    virtual std::string getProduto() const;
+    virtual double getValor() const;
+    virtual void vaifazeradesgraca() = 0;
+};
 
 
 
